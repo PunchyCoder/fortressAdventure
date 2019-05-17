15eb3489715d133bcb4e1e31e3f032aacb0ca86f
PunchyCoder <jissensmasher@gmail.com>
2019-05-17 13:12:32 -0700

    Add 'door' command
    Add 'door' command
    Add comments to main.js
    Add location data to display
    Add entrance, entryway, small garden to data.js

--- data.js
+++ data.js
@@ -1,3 +1,32 @@
-const scene = {
-	message: "You've been running from something and find yourself lost and in front of some Godforsaken door in a tree.."
+ const scene = {
+ 	entrance: {
+ 		name: "Entrance",
+ 		message: {
+ 			'1': "You've been running from something and find yourself lost and in front of some Godforsaken door in a tree..",
+ 			'2': "You light a torch, to see where you've found yourself...",
+ 			'3': "A massive door stands before you. Like something out of a horror story, a set of hollow sockets glow red from the skull-shaped keystone above the door. They seem to be watching you.",
+ 			'4': "The door, fortified with steel bars, has a ring pull and a keyhole."
+
+ 		},
+ 		door: function() {
+ 			console.log("this is a door.")
+ 		}
+ 	},
+
+ 	entryWay: {
+ 		name: "The Entryway",
+ 		message: {
+ 			'1': "When you walk in, you are greeted by the smell of death.",
+ 			'2': "The light of your torch feels like it's being constrained. It's almost like the darkness is trying to crush you under it's weight. It would be a good idea to to always keep a torch lit, lest you test your fate."
+ 		}
+ 	},
+
+ 	smallGarden: {
+ 		name: "Small Garden",
+ 		message: {
+ 			'1': "Once you open the door, you are greeted by a small garden with a fountain and a well."
+ 		}
+ 	}
+
+
 }
--- main.js
+++ main.js
@@ -1,33 +1,57 @@
-
+ 
 
 function addImage(item) {
 	$('.pics').append(`<img src="https://media.giphy.com/media/11BgMeY4SMOAEg/giphy.gif" height="80" width="80">`);
 } 
 
-function useTorch() { // should add a picture of a torch flickering 
-	$('body').css('background-color', 'yellow'); // what if when a torch is in use, the background is permanently yellow(or other color)??
-	setTimeout(()=> $('body').css('background-color', 'black'), 120) // flashes the background yellow
+function useTorch() { 
+	// add a picture of a torch flickering 
+	// what if when a torch is in use, the background is permanently yellow(or other color)??
+	$('body').css('background-color', 'yellow'); 
+	// flash the background yellow
+	setTimeout(()=> $('body').css('background-color', 'black'), 120) 
 	addImage();
 }
 
-function enterCommand() {
+function resetCommand() {
+	//reset input value
+	$('#command-input').val(''); 
+}
+
+function enterCommand(location) {
 	const input = $('#command-input').val();
+
+	//enter torch command
 	if (input == 'torch') {
 		useTorch();
-	};
-	$('#command-input').val(''); //reset input value
+	}
+
+	// enter door command
+	if (input == 'door') {
+		location.door(); 
+	}
+
+	resetCommand();
 }
 
 function message(textHTML) {
-	$('.container').append(`<p class="message"></p>`);
+	$('.container').append(`<p class="message box"></p>`);
 	$('.message').text(textHTML);
 }
 
 function watchForm() {
-	message(scene.message);
+	// push and pop new locations
+	const playerLocation = [];
+	playerLocation.push(scene.entrance); 
+
+	 // add location data to display
+	$('#location').append(`<h2 class='box'>${playerLocation[0]['name']}</h2>`);
+
+	// add Submit event listener
+	message(scene.entrance.message[1]);
 	$('form').submit(function() {
 		event.preventDefault();
-		enterCommand();
+		enterCommand(playerLocation[0]);
 	})
 
 }
